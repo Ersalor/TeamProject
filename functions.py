c2066008fbdb0665cd993bc956d9406e62a5eebb
Ersalor <mehmet.z.sanli@gmail.com>
2025-05-08 18:15:19 +0300

    Çalışma alanı oluşturuldu (functions.py)

--- functions.py
+++ functions.py
@@ -1,1 +1,20 @@
 #All functions should be here
+
+
+def main():
+    pass
+
+# ↓↓↓ Ersalor's work space ↓↓↓ 
+
+
+
+# ↓↓↓ Mehmetcan's work space ↓↓↓
+
+
+
+# ↓↓↓ Hasan's work space ↓↓↓
+
+
+
+if __name__=="main":
+    main()
