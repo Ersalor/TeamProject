5416028554c5bdd348181eb89ab6d8b7eb060d5c
mcuzkan <mehmetcanuzkan@gmail.com>
2025-05-09 14:36:15 +0300

    eklemefalan

--- functions.py
+++ functions.py
@@ -10,7 +10,15 @@ def main():
 
 # ↓↓↓ Mehmetcan's work space ↓↓↓
 
+def AllNeighboodsList():
+    with open("neighborhoods.txt","r",encoding="utf-8") as rows:
+        list = []
+        for line in rows:
+            list.append(line.strip())
+        return list
 
+for line in AllNeighboodsList():    
+    print(line)
 
 # ↓↓↓ Hasan's work space ↓↓↓
 
